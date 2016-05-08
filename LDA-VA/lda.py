1b77af9793ef610e2955e98b5a64d0cf7260ad1d
GUOKUN LAI <laiguokun@163.com>
2016-05-08 17:07:29 +0800

    lda-va by python

--- LDA-VA/lda.py
+++ LDA-VA/lda.py
@@ -1,5 +1,4 @@
 from readfile import readfile;
-import numpy as np;
 import scipy.special as sp;
 import math;
 import time;
@@ -16,26 +15,49 @@ doc_size = 0;
 voca_size = 0;
 timer = 0;
 
+def sum_vector(a):
+	res = 0;
+	for i in range(len(a)):
+		res += a[i];
+	return res;
+
+def sum_matrix(a, op):
+	res = []
+	n = len(a);
+	m = len(a[0]);
+	if (op == 0):
+		for j in range(m):
+			res.append(0);
+			for i in range(n):
+				res[j] += a[i][j];
+
+	return res;
+
 def init():
 	global alpha,beta,Gamma,Phi,doc,doc_cnt;
 
 	#random start
+	beta = [];
 	for i in range(K):
-	beta = np.ones((K,voca_size));
-	beta = beta/K;
-	for v in range(voca_size):
+		beta.append([]);
 		tot = 0;
-		for i in range(K):
-			beta[i][v] += random.random()/10;
-			tot += beta[i][v];
-		for i in range(K):
-			beta[i][v] = beta[i][v] / tot;
+		for j in range(voca_size):
+			beta[i].append(1/K + random.random()/10);
+			tot += beta[i][j];
+		for j in range(voca_size):
+			beta[i][j] /= tot;
 
 	Phi = [];
 	Gamma = [];
 	for d in range(doc_size):
-		Phi.append(np.ones((len(doc[d]), K))/K);
-		Gamma.append(np.ones(K));
+		Phi.append([]);
+		for n in range(len(doc[d])):
+			Phi[d].append([]);
+			for i in range(K):
+				Phi[d][n].append(1/K);
+		Gamma.append([]);
+		for i in range(K):
+			Gamma[d].append(1/K + alpha);
 	alpha = 1;
 
 def compute_mle(d):
@@ -43,7 +65,7 @@ def compute_mle(d):
 	res = 0;
 	res += sp.gammaln(K * alpha);
 	res -= K * sp.gammaln(alpha);
-	gamma_sum = np.sum(Gamma[d]);
+	gamma_sum = sum_vector(Gamma[d]);
 	length = len(doc[d]);
 	psi = [];
 
@@ -60,8 +82,8 @@ def compute_mle(d):
 	for n in range(length):
 		for i in range(K):
 			res += doc_cnt[d][n] * Phi[d][n][i] * psi[i];
-			res -= doc_cnt[d][n] * Phi[d][n][i] * math.log(Phi[d][n][i]);
-			res += doc_cnt[d][n] * Phi[d][n][i] * math.log(beta[i][doc[d][n]]);
+#			res -= doc_cnt[d][n] * Phi[d][n][i] * math.log(Phi[d][n][i]);
+			res += doc_cnt[d][n] * Phi[d][n][i] * math.log(beta[i][doc[d][n]]/Phi[d][n][i]);
 
 	timer += time.time() - now;
 	res -= sp.gammaln(gamma_sum);
@@ -74,11 +96,11 @@ def compute_alpha_mle(alpha):
 	for d in range(doc_size):
 		res += sp.gammaln(K * alpha);
 		res -= K * sp.gammaln(alpha);
-		gamma_sum = np.sum(Gamma[d]);
+		gamma_sum = sum_vector(Gamma[d]);
 		for i in range(K):
 			const += (sp.psi(Gamma[d][i]) - sp.psi(gamma_sum));	
 	res += (alpha - 1) * const;
-	return res, const; 
+	return -res; 
 
 
 def mle():
@@ -87,13 +109,15 @@ def mle():
 	timer = 0;
 	for d in range(doc_size):
 		res += compute_mle(d);
-	print(timer);
 	return res;
 
 def Estep(d, max_iter):
 	global alpha,beta,Gamma,Phi,doc,doc_cnt;
-	Phi[d] = np.ones((doc_size, K)) / K;
-	Gamma[d] = np.ones(K) * len(doc[d]) / K + alpha;
+	for n in range(len(doc[d])):
+		for i in range(K):
+			Phi[d][n][i] = 1 / K;
+	for i in range(K):
+		Gamma[d][i] = 1/K + alpha;
 	last = 0;
 	now = compute_mle(d);
 	origin = now;
@@ -102,62 +126,73 @@ def Estep(d, max_iter):
 
 		gamma_sum = sum(Gamma[d]);
 		for n in range(len(doc[d])):
+			tot = 0;
 			for i in range(K):
 				Phi[d][n][i] = doc_cnt[d][n] * beta[i][doc[d][n]] * math.exp(sp.psi(Gamma[d][i]) - sp.psi(gamma_sum));
-			Phi[d][n] = Phi[d][n] / np.sum(Phi[d][n]);
+				tot += Phi[d][n][i];
+			for i in range(K):
+				Phi[d][n][i] = Phi[d][n][i] / tot;
 		for i in range(K):
 			Gamma[d][i] = 0;
 			for n in range(len(doc[d])):
 				Gamma[d][i] += doc_cnt[d][n] * Phi[d][n][i];
-		Gamma[d] += alpha;
+			Gamma[d][i] += alpha;
 		last = now;
-		now = last + 1;
-#		now = compute_mle(d);
+		now = compute_mle(d);
 		iter_num += 1;
 
 	if (now < origin):
 		print('error ' + str(d));
 
-def backtrack(alpha,dx,da):
+def backtrack(x,dx,df,alpha,beta):
 	t = 1;
-	while (compute_alpha_mle(alpha + t * dx) > compute_alpha_mle(alpha) + alpha * t * da * dx):
+	while (x + t * dx < 0 or x + t * dx > 1 or compute_alpha_mle(x + t * dx) > compute_alpha_mle(x) + alpha * t * df * dx):
 		t = t * beta;
-	alpha = alpha + t * dx;
-	return alpha;
+	x = x + t * dx;
+	return x;
 
 	
 def Mstep(max_iter):
 	global alpha,beta,Gamma,Phi,doc,doc_cnt;
 	#update beta
-	beta = np.zeros((K,voca_size));
 	for i in range(K):
+		for v in range(voca_size):
+			beta[i][v] = 0;
 		for d in range(doc_size):
 			for n in range(len(doc[d])):
 				beta[i][doc[d][n]] += doc_cnt[d][n] * Phi[d][n][i];
-	beta_sum = np.sum(beta, axis = 0);
+	beta_sum = sum_matrix(beta, 0);
 	for i in range(voca_size):
-		beta[:,i] = beta[:,i]/beta_sum[i];
+		for k in range(K):
+			beta[k][i] = beta[k][i]/beta_sum[i];
 	#update alpha
 	last = 0;
+	iter_num = 0;
 	const = 0;
-	now, const = compute_alpha_mle(alpha);
+	for d in range(doc_size):
+		gamma_sum = sum_vector(Gamma[d]);
+		for i in range(K):
+			const += (sp.psi(Gamma[d][i]) - sp.psi(gamma_sum));	
+	now = -compute_alpha_mle(alpha);
+	origin = now;
 	while (abs(last - now) > 1e-9 and iter_num < max_iter):
 		da = K * (doc_size * (sp.psi(alpha * K) - sp.psi(alpha))) + const;
 		dda = K * (doc_size * (K * sp.polygamma(1, alpha * K) - sp.polygamma(1, alpha)));
 		dx = -da/dda;
-		alpha = backtrack(alpha,dx,da);
+		alpha = backtrack(alpha,dx,da,0.01,0.5);
 		last = now;
-		now, const = compute_alpha_mle(alpha);
+		now = -compute_alpha_mle(alpha);
 		iter_num += 1;
+	if (now < origin):
+		print('error alpha');
 
-
-def savemodel():
+def savemodel(num):
 	global alpha,beta,Gamma,Phi,doc,doc_cnt;
-	fout = open('lda_model.txt', 'w');
+	fout = open('lda_model'+ str(num) + '.txt', 'w');
 	fout.write(str(alpha) + '\n');
 	for i in range(K):
 		for v in range(voca_size):
-			fout.write(beta[i][v] + ' ');
+			fout.write(str(beta[i][v]) + ' ');
 		fout.write('\n');
 
 
@@ -176,7 +211,8 @@ def train(max_iter):
 		print(now);
 		print('Mstep');
 		Mstep(20); # the m step of em algorith
-	savemodel();
+		print(alpha);
+		savemodel(i);
 
 
 if __name__ == '__main__':
@@ -185,4 +221,4 @@ if __name__ == '__main__':
 	voca_size = len(voca);
 	doc_size = len(doc);
 	init();
-	train(1);
+	train(10);
--- LDA-VA/readfile.py
+++ LDA-VA/readfile.py
@@ -21,11 +21,13 @@ class readfile:
 				tmp = words[i].split(':');
 				if (not tmp[0] in self.voca):
 					self.voca[tmp[0]] = len(self.voca);
-				d.append(int(tmp[0]));
+				d.append(int(self.voca[tmp[0]]));
 				d_cnt.append(int(tmp[1]));
 			self.doc.append(d);
 			self.doc_cnt.append(d_cnt);
 			cnt += len(d);
+			if (len(self.doc) > 100):
+				break;
 		print('document length:' + str(cnt));
 		print('documnet num:' + str(len(self.doc)));
 		return self.doc, self.voca, self.doc_cnt;
